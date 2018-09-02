0c6386b72e6eee4e5024bc70b98079016a547721
Steven Zapata <szapata427@gmail.com>
2018-09-02 18:56:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,6 @@ def assign_rooms(speakers)
  end
  
  def printer(attendees)
-   batch_badge_creator(attendees)
-   assign_rooms(attendees)
+   puts batch_badge_creator(attendees)
+ puts assign_rooms(attendees)
  end
